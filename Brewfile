46f5ecb236c8042eba1848030d863577e85ab529
Francois Buys <buys.fran@gmail.com>
2021-05-24 15:04:15 +0200

    Use Homebrew Bundle to manage brew dependencies

---
+++ Brewfile
@@ -0,0 +1,36 @@
+tap "candid82/brew"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# apps
+cask "firefox"
+cask "google-chrome"
+cask "slack"
+
+# dev
+brew "curl"
+brew "fzf"
+brew "git"
+brew "ripgrep"
+brew "tmux"
+brew "tmux-mem-cpu-load"
+brew "yqrashawn/goku/goku"
+brew "zsh-completions"
+cask "iterm2"
+cask "karabiner-elements"
+tap "yqrashawn/goku"
+
+# asdf
+brew "asdf"
+brew "coreutils"
+
+# node
+brew "gnupg"
+brew "gawk"
+
+# ruby
+brew "shared-mime-info"
+
+# redis
+brew "redis"
--- mac.sh
+++ mac.sh
@@ -23,46 +23,19 @@ fi
 if [[ `uname -m` == 'arm64' ]]; then
   softwareupdate --install-rosetta
 fi
+script_dir=$(pwd) # store current working directory
 
 println() {
   printf "%b\n" "$1"
 }
 
-brew_install_or_upgrade() {
-  if brew_is_installed "$1"; then
-    if brew_is_upgradable "$1"; then
-      brew upgrade "$@"
-      println "Upgraded $1"
-    else
-      println "$1 already installed"
-    fi
-  else
-    brew install "$@"
-  fi
-}
-
-brew_expand_alias() {
-  brew info "$1" 2>/dev/null | head -1 | awk '{gsub(/:/, ""); print $1}'
-}
-
-brew_is_installed() {
-  local NAME=$(brew_expand_alias "$1")
-
-  brew list -1 | grep -Fqx "$NAME"
-}
-
-brew_is_upgradable() {
-  local NAME=$(brew_expand_alias "$1")
-
-  local INSTALLED=$(brew ls --versions "$NAME" | awk '{print $NF}')
-  local LATEST=$(brew info "$NAME" 2>/dev/null | head -1 | awk '{gsub(/,/, ""); print $3}')
-
-  [ "$INSTALLED" != "$LATEST" ]
-}
-
 if ! command -v brew &>/dev/null; then
   println "The missing package manager for OS X"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  if [ ! -f "$HOME/.bashrc" ]; then
+    touch $HOME/.bashrc
+  fi
 
   if ! grep -qs "recommended by brew doctor" ~/.bashrc; then
     println "Put Homebrew location earlier in PATH..."
@@ -74,7 +47,6 @@ else
   println "Homebrew already installed. Skipping..."
 fi
 
-# Start of installation...
 
 # Symlink dotfiles
 dir=./dotfiles
@@ -82,21 +54,18 @@ home_files=".vimrc .tmux.conf .zshrc .gitconfig .gitignore .default-npm-packages
 config_files="karabiner.edn"
 ctags_files="md.ctags"
 
-if [[ -d $dir ]]
-then
+if [[ -d $dir ]]; then
   cd $dir
   for file in $home_files; do
-    if [[ -f $file ]]
-    then
-      echo "Creating symlink to $file in home directory."
+    if [[ -f $file ]]; then
+      println "Creating symlink to $file in home directory."
       ln -sf $(pwd)/$file ~/$file
     fi
   done
 
   for file in $config_files; do
-    if [[ -f $file ]]
-    then
-      echo "Creating symlink to $file in home/.config directory."
+    if [[ -f $file ]]; then
+      println "Creating symlink to $file in home/.config directory."
       ln -sf $(pwd)/$file ~/.config/$file
     fi
   done
@@ -120,89 +89,39 @@ then
   for file in $hooks; do
     if [[ -f $file ]]
     then
-      echo "Creating symlink to $file in git template directory."
+      println "Creating symlink to $file in git template directory."
       ln -sf $(pwd)/$file ~/$dir/$file
     fi
   done
 fi
 
 # Brew
-println "Updating Homebrew formulas..."
-brew update
+if [[ -z "${UPGRADE_BREW}" ]]
+then
+  println "Updating Homebrew formulas..."
+  brew update
+  brew upgrade
+  brew bundle --file $script_dir/BrewFile
+fi
 
-println "Upgrade existing Homebrew formulas..."
-brew upgrade
-
-println "Installing Git..."
-brew_install_or_upgrade 'git'
-
-println "Installing CoreUtiles (asdf dependency)..."
-brew_install_or_upgrade 'coreutils'
-
-println "Installing curl (asdf dependency)..."
-brew_install_or_upgrade 'curl'
-
-println "Installing gpg (node dependency)..."
-brew install gpg
-
-println "Installing gawk (node dependency)..."
-brew install gawk
-
-println "Installing iTerm2..."
-brew install --cask iterm2
-
-println "Installing Tmux..."
-brew install tmux
-brew install tmux-mem-cpu-load
-
-println "Installing Chrome..."
-brew reinstall --cask google-chrome
-
-println "Installing Firefox..."
-brew reinstall --cask firefox
-
-println "Installing Slack..."
-brew reinstall --cask slack
-
-println "Installing Postgres..."
-brew_install_or_upgrade 'postgres'
-
-println "Installing Postgres.app..."
-brew reinstall --cask postgres
-
-println "Installing Redis..."
-brew_install_or_upgrade 'redis'
-
-println "Installing Universal Ctags..."
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
-
-println "Installing Karabiner elements..."
-brew install --cask karabiner-elements
-brew install yqrashawn/goku/goku
-goku
-
-# Install asdf + plugins
-println "Installing asdf..."
-brew_install_or_upgrade 'asdf'
-
+# asdf  plugins
+println "Installing asdf plugins..."
 asdf plugin add nodejs
 asdf install nodejs 14.16.1
 asdf global nodejs 14.16.1
 
 asdf plugin-add yarn
-asdf install yarn lates
-# To make sure yarn packages are globally accessible
-# yarn config set prefix ~/.yarn
+asdf install yarn latest
+yarn config set prefix ~/.yarn # To make sure yarn packages are globally accessible
 
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf install ruby 3.0.1
 asdf install ruby 2.7.3
-asdf global ruby 14.16.1
+asdf global ruby 3.0.1
 
 # zshell and oh-my-zshell
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   println "Installing oh my zshell..."
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  println "Installing zshell autosuggestions..."
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
