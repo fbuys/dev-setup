96e911a43d4402f9429588b7e9044492e41200b8
Francois Buys <buys.fran@gmail.com>
2023-06-08 00:30:39 +0200

    Remove which key
    It seems to slow the editor down.

--- nvim/lua/fbuys/packer.lua
+++ nvim/lua/fbuys/packer.lua
@@ -174,18 +174,18 @@ require('packer').startup(function(use)
   }
 
   -- Show possible keybindings
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
+  -- use {
+  --   "folke/which-key.nvim",
+  --   config = function()
+  --     vim.o.timeout = true
+  --     vim.o.timeoutlen = 300
+  --     require("which-key").setup {
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       -- refer to the configuration section below
+  --     }
+  --   end
+  -- }
 
   -- LSP formatting 
   -- Removing it because it slows my editor down
