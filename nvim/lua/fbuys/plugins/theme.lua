8ee08d7cdd9027611c0ec03fbe01f774dd40c7a9
Francois Buys <buys.fran@gmail.com>
2024-04-29 18:45:07 +0200

    Make vim transparent

--- nvim/lua/fbuys/plugins/theme.lua
+++ nvim/lua/fbuys/plugins/theme.lua
@@ -1,11 +1,17 @@
 -- Color theme
 return {
+  { -- Theme
   "rose-pine/neovim",
   name = "rose-pine",
   priority = 999,
   config = function()
     require("rose-pine").setup({
       variant = "moon",
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
     })
 
     function ColorMyPencils(color)
@@ -18,4 +24,21 @@ return {
 
     ColorMyPencils()
   end,
+  },
+  { -- Remove all background colors to make nvim transparent
+    "xiyaowong/transparent.nvim",
+    config = function ()
+      require("transparent").setup({ -- Optional, you don't have to run setup.
+        groups = { -- table: default groups
+          'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+          'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+          'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+          'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+          'EndOfBuffer',
+        },
+      extra_groups = {}, -- table: additional groups that should be cleared
+      exclude_groups = {}, -- table: groups you don't want to clear
+    })
+    end,
+  },
 }
