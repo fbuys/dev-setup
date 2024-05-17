ae1598bc67a66c9ab6d70256d6f9a3dc8be1ea2d
Francois Buys <buys.fran@gmail.com>
2024-05-17 23:57:38 +0200

    Improve nvim-lint config

--- nvim/lua/fbuys/plugins/lint.lua
+++ nvim/lua/fbuys/plugins/lint.lua
@@ -1,15 +1,21 @@
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+
+    -- You can call `try_lint` with a linter name or a list of names to always
+    -- run specific linters, independent of the `linters_by_ft` configuration
+    -- require("lint").try_lint("cspell")
+  end,
+})
+
 return {
-  'mfussenegger/nvim-lint',
+  "mfussenegger/nvim-lint",
   config = function()
-
-    require('lint').linters_by_ft = {
-      ruby = {'rubocop',}
+    require("lint").linters_by_ft = {
+      ruby = { "rubocop" },
+      lua = { "luacheck" },
     }
-
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
   end,
 }
